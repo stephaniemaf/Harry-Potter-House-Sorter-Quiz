e4154754d39b8f44c41a21f0de61ebb28ce87076
stephaniemaf <farrell.s.ava@gmail.com>
2022-12-14 12:38:49 +0000

    added input statement to get name of user and added that name tp the welcome message

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 """Welcome message for quiz game"""
 
+name = input("what is you name: ")
+
+
 print("")
 print("*" *80)
-print("Welcome to the Harry Potter sorting quiz. Answer these 7 questions")
+print(f"Welcome {name} to the Harry Potter sorting quiz. Answer these 7 questions")
 print("honestly, and at the end of this quiz you will be sorted into your")
 print("Hogwarts House where you will spend the next 7 years studying magic")
 print("at the greatest school of Witchcraft and Wizardry. Best of luck!!")
 print("")
 print("*" *80)
+
+
+
+
 
 
