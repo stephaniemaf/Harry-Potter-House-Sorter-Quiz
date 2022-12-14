bbab333fa616b3a9ef2930d51ca21ef0a77dae08
stephaniemaf <farrell.s.ava@gmail.com>
2022-12-14 13:50:31 +0000

    added house variables that will later be used to decide wich house user belongs in also added code to open and readd txt file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-"""Welcome message for quiz game"""
+
+"""Welcome message for quiz game asking for name and inserting it into said message"""
 
 name = input("what is you name: ")
 
@@ -12,8 +13,16 @@ print("at the greatest school of Witchcraft and Wizardry. Best of luck!!")
 print("")
 print("*" *80)
 
+def open_questions():
+    quiz_questions = []
 
+    with open("requirements.txt", "r") as f:
+        
+        questions = f.readlines()
+        for quest in questions:
+            quiz_questions.append(quest.strip())
+    return quiz_questions
 
-
-
-
+quiz_questions = open_questions()
+for question in quiz_questions:
+    print("Question1")
