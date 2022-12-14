e1da04ee68f47e353aed904721f61489bfc6f936
stephaniemaf <farrell.s.ava@gmail.com>
2022-12-14 16:52:19 +0000

    wrapped welcome message in a function

--- run.py
+++ run.py
@@ -1,28 +1,24 @@
+gryffindor = 0
+hufflepuff = 0
+ravenclaw = 0
+slytherin = 0
 
-"""Welcome message for quiz game asking for name and inserting it into said message"""
 
-name = input("what is you name: ")
+def welcome_message():
+    """
+    Welcome message for quiz game asking for name and inserting it into said message
+    """
+
+    name = input("what is you name: ")
 
 
-print("")
-print("*" *80)
-print(f"Welcome {name} to the Harry Potter sorting quiz. Answer these 7 questions")
-print("honestly, and at the end of this quiz you will be sorted into your")
-print("Hogwarts House where you will spend the next 7 years studying magic")
-print("at the greatest school of Witchcraft and Wizardry. Best of luck!!")
-print("")
-print("*" *80)
+    print("")
+    print("*" *80)
+    print(f"Welcome {name} to the Harry Potter sorting quiz. Answer these 7 questions")
+    print("honestly, and at the end of this quiz you will be sorted into your")
+    print("Hogwarts House where you will spend the next 7 years studying magic")
+    print("at the greatest school of Witchcraft and Wizardry. Best of luck!!")
+    print("")
+    print("*" *80)
 
-def open_questions():
-    quiz_questions = []
-
-    with open("requirements.txt", "r") as f:
-        
-        questions = f.readlines()
-        for quest in questions:
-            quiz_questions.append(quest.strip())
-    return quiz_questions
-
-quiz_questions = open_questions()
-for question in quiz_questions:
-    print("Question1")
+welcome_message()
