4929a1aaec0bed5336d906db54617aa648f9b212
stephaniemaf <farrell.s.ava@gmail.com>
2022-12-15 14:40:16 +0000

    added for loop above if statement to ask user a question then ask for theyre answer, once answered its stored in the corresponding variable and the next question is answered. Questions are stored in a list of dictionaries

--- run.py
+++ run.py
@@ -32,24 +32,45 @@ def questions_for_quiz():
     ravenclaw = 0
     slytherin = 0
 
+    questions = [{
+                    "question": "How would you like to be known in history?",
+                    "choices": ["a) The wise", "b) The good", "c) The bold",
+                    "d) The Great"
+                ]},
+                {
+                    "question": "Which Quality best describes you?",
+                    "choices": ["a) Brave","b) Intelligent","c) Ambitious",
+                    "d) Empathetic"
+                ]},
+                {
+                    "question": "Which potion would you rather have?",
+                    "choices": ["a) Glory", "b) Wisdom", "c) Love", "d) Power",
+                ]},
+                {
+                    "question": "You're trapped in a burning building.What do you do?",
+                    "choices": ["a) Run and Grab your friend thats also trapped", 
+                    "b) Save yourself, of course!!", "c) Give it a shot but your not sure you can save your friend",
+                    "d) It depends if you can save your friend you will, if not...your just gonna run."]
+                }]
+              
+    for question in questions:
+        print(question["question"])
+        print("")
+        print("Choices:")
+        for i in question["choices"]:
+            print(i)
 
-    questions = [{"question": "How would you like to be known in history?",
-                    "choices": ["The wise", "The good", "The bold", "The Great"
-                    ]}]
-
-    print(questions)
-
-    guess = input("Please enter you answer: ")
-    if guess == "a":
-        ravenclaw += 1
-    elif guess == "b":
-        hufflepuff += 1
-    elif guess == "c":
-        gryffindor += 1
-    elif guess == "d":
-        slytherin += 1
-    else:
-        print("Unknown entry please try again")
+        guess = input("Please enter you answer: ")
+        if guess == "a":
+            ravenclaw += 1
+        elif guess == "b":
+            hufflepuff += 1
+        elif guess == "c":
+            gryffindor += 1
+        elif guess == "d":
+            slytherin += 1
+        else:
+            print("Unknown entry please try again")
 
 
 welcome_message()
